00b2a29e9005f61594654cfa6f1fc9dc2923be84
ComputerGAP <ComputerGAP@192.168.0.9>
2018-05-22 17:53:58 -0500

    add webform

---
+++ src/main/java/servlet/WebForm.java
@@ -0,0 +1,27 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+        name = "webForm", 
+        urlPatterns = {"/webForm"}
+    )
+public class WebForm extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+		response.sendRedirect("page.jsp?urlAction=decodePaymentResponseServlet&publicMerchantId=10000002825331705579151380634343&paymentNumOrder=");
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(req, resp);
+	}
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,14 @@
-<html>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html><head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+</head>
 <body>
-<h2>Hello Heroku!</h2>
+<form action="webForm" id="indexForm" method="get">
+	<input type="text" id="urlAction" name="urlAction" placeholder="UrlAction" value="decodePaymentResponseServlet">
+	<input type="text" id="publicMerchantId" name="publicMerchantId" placeholder="publicMerchantId" value="10000002825331705579151380634343">
+	<input type="submit" value="Next"/>
+</form>
 </body>
 </html>
